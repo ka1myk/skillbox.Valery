ce83c8df8dad408fd5e899988759df958dce206e
ka1myk <you@example.com>
2019-03-02 07:40:06 +0300

    03/02/19 07:40:06

--- Веб-вертска/Дипломный проект/ДЗ/script.js
+++ Веб-вертска/Дипломный проект/ДЗ/script.js
@@ -1,29 +1,5 @@
-$(function(){
-		
-	$("#selectcity").click(function(){
-		$("#popup-container").fadeIn(400);
-		$("#popup").animate({
-			width: '200px',
-			height: '200px'
-		},400);
-		scrollLock.hide();
-	});
-
-	$("#popup-container").click(function(event){
-		if(event.target == this) {
-			$(this).fadeOut(400);
-			$("#popup").animate({
-				width: 0,
-				height: 0
-			},400);
-			scrollLock.show();
-		}		
-	});
+$(function(){	
 	
-		$.get('http://data.fixer.io/api/latest?access_key=dc3fde92810182d80b772b3019f22ec2&base=EUR&symbols=RUB', function(data){
-			document.getElementById("currencyEUR").innerHTML += "<p>EUR MOEX</p>"+data.rates.RUB.toFixed(2);
-						
-		});
 
 		function menuFunction(x) {
 		  if (x.matches) { // If media query matches
@@ -36,18 +12,7 @@ $(function(){
 		var x = window.matchMedia("(max-width: 950px)")
 		menuFunction(x) // Call listener function at run time
 		x.addListener(menuFunction) // Attach listener function on state changes
-
-		function searchFunction(xx) {
-		  if (xx.matches) { // If media query matches
-		    $('#search-line').hide();
-		  } else {
-		    $('#search-line').show();
-		  }
-		}
-
-		var xx = window.matchMedia("(max-width: 530px)")
-		searchFunction(xx) // Call listener function at run time
-		xx.addListener(searchFunction) // Attach listener function on state changes
+	
 
 		if (window.matchMedia('(max-width: 950px)').matches)
 		{
@@ -67,30 +32,7 @@ $(function(){
 
 		        return false;
 		    });
-		}
-			
-
-		if (window.matchMedia('(max-width: 530px)').matches)
-		{
-		    $('#search-bt').on('click', function()
-		    {
-		        var sline = $('#search-line');
-		        var logo = $('#logo');
-		        if(sline.is(':visible'))
-		        {
-		           sline.hide();
-		           logo.show()
-		        } 
-		        else {
-		           sline.show();
-		           logo.hide();
-		        }
-
-		        return false;
-		    });
-		}
-
-
+		}		
 
 	});
 
